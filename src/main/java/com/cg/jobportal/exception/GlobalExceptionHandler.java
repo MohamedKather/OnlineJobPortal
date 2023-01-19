6bfd3cce6a4cd41d07019bd3d2069cb0758a7c33
Attar <shifa-khanam.attar@capgemini.com>
2023-01-19 14:08:52 +0530

    added

--- src/main/java/com/cg/jobportal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cg/jobportal/exception/GlobalExceptionHandler.java
@@ -5,6 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+
+
+
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
@@ -12,4 +16,12 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<String> userAlreadyExistException(UserAlreadyExistException msg){
 		return new ResponseEntity<String>("User Already Exist,Please Try Again.",HttpStatus.CONFLICT);
 	}
+	@ExceptionHandler(value=BookmarkedFreelancerAlreadyExistsException.class)
+	public ResponseEntity<String> BookmarkedFreelancerAlreadyExistsException(BookmarkedFreelancerAlreadyExistsException msg){
+		return new ResponseEntity<String> ("Bookmarked freelancer already exists, please try with another freelancer", HttpStatus.CONFLICT);
+	}
+	@ExceptionHandler(value=NoBookmarkedFreelancerExistsException.class)
+	public ResponseEntity<String> NoBookmarkedFreelancerExistsException(NoBookmarkedFreelancerExistsException msg){
+		return new ResponseEntity<String> (" No bookmarked freelancer exists in db", HttpStatus.CONFLICT);
+	} 
 }
